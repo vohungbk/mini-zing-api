05eaa03a71c8e130d84d240ac985013b8ec055d2
Hung Vo <vohungbk@gmail.com>
2022-07-08 11:31:41 +0700

    Update controller

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
-const path = require('path')
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const port = process.env.PORT || 3000
+const path = require("path");
+const jwt = require('jsonwebtoken');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+
+const authenticateJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  console.log('alo',{authHeader});
+
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, accessTokenSecret, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+
+      req.user = user;
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
 
 // Page Home
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'))
-})
+app.get("/", authenticateJWT, (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
 
 // ZingMp3Router
-const ZingMp3Router = require("./routers/api/ZingRouter")
-app.use("/api", cors(), ZingMp3Router)
+const ZingMp3Router = require("./routers/api/ZingRouter");
+app.use("/api", cors(), ZingMp3Router);
 
 // Page Error
 app.get("*", (req, res) => {
-  res.send("Nhập Sai Đường Dẫn! Vui Lòng Nhập Lại >.<")
+  res.send("Nhập Sai Đường Dẫn! Vui Lòng Nhập Lại >.<");
 });
 
 app.listen(port, () => {
-  console.log(`Start server listen at http://localhost:${port}`)
+  console.log(`Start server listen at http://localhost:${port}`);
 });
